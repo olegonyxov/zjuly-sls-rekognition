88c8a752c90aff6d1f004b56fe9c2eaeb5a65570
olegonyxov <olegonyxov@gmail.com>
2022-07-31 18:25:28 +0300

    +read

--- src/1create.py
+++ src/1create.py
@@ -1,22 +1,47 @@
-import json
 import os
-import random,string
+import json
 import boto3
-from botocore.config import Config
+import string
+import random
 
+dynamodb_client = boto3.client("dynamodb")
+TABLE_NAME = os.environ["TABLE_NAME"]
+DNS_RECORD = os.environ["DNS_RECORD"]
 
-BUCKET_NAME= os.environ["BUCKET_NAME"]
-s3 = boto3.client(
-        's3',
-        region_name='us-east-1',
-        aws_access_key_id='AKIAZYKTPU7IB2XXD7VR',
-        aws_secret_access_key='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',
-        config=Config(signature_version='s3v4'),)
 
 def handler(event, context):
-    blob_id = ''.join(random.choices(string.ascii_lowercase + string.digits, k=6))
-    url= s3.generate_presigned_url('put_object', Params={'Bucket': BUCKET_NAME, 'Key': blob_id},
-                                  ExpiresIn=3600, HttpMethod='PUT')
-    print(url)
-    return url
+    # get body from event
+    event_body = event.get("body")
 
+    # body validation
+    if not event_body:
+        return {"statusCode": 400,
+                "body": json.dumps({"error": "body is empty"})}
+
+    # deserialization of request body
+    request_body = json.loads(event_body)
+
+    # long_url validation
+    long_url = request_body.get("long_url")
+    if not long_url:
+        return {"statusCode": 400,
+                "body": json.dumps({"error": "param long_url required"})}
+
+    # generate primary key for DynamoDB table
+    url_id = ''.join(random.choices(string.ascii_lowercase + string.digits, k=6))
+
+    # add a new item to the URL_TABLE
+    dynamodb_client.put_item(
+        TableName=TABLE_NAME,
+        Item={
+            "url_id": {"S": url_id},
+            "long_url": {"S": long_url}})
+
+    # create short_url
+    short_url = DNS_RECORD + url_id
+
+    # return short_url
+    return {
+        "statusCode": 200,
+        "body": json.dumps({"url_id": url_id,
+                            "short_url": short_url})}
--- src/create.py
+++ src/create.py
@@ -6,6 +6,8 @@ from botocore.config import Config
 
 
 BUCKET_NAME= os.environ["BUCKET_NAME"]
+TABLE_NAME= os.environ["TABLE_NAME"]
+dynamodb_client = boto3.client("dynamodb")
 s3 = boto3.client(
         's3',
         region_name='us-east-1',
@@ -14,13 +16,36 @@ s3 = boto3.client(
         config=Config(signature_version='s3v4'),)
 
 def handler(event, context):
+    # get body from event
+    event_body = event.get("body")
+
+    # body validation
+    if not event_body:
+        return {"statusCode": 400,
+                "body": json.dumps({"error": "body is empty"})}
+
+    # deserialization of request body
+    request_body = json.loads(event_body)
+
+    # callback_url validation
+    callback_url = request_body.get("callback_url")
     # get a new name
     blob_id = ''.join(random.choices(string.ascii_lowercase + string.digits, k=6))
     # get URL
     url= s3.generate_presigned_url('put_object', Params={'Bucket': BUCKET_NAME, 'Key': blob_id},
                                   ExpiresIn=3600, HttpMethod='PUT')
     #create blob_url
-    blob_url= "s3://"+BUCKET_NAME+"/"+blob_id
+    blob_url= "arn:aws:s3://"+BUCKET_NAME+"/"+blob_id
 
-    return "lalalla"
+    # add a new item to the URL_TABLE
+    dynamodb_client.put_item(
+        TableName=TABLE_NAME,
+        Item={
+            "blob_id": {"S": blob_id},
+            "callback_url": {"S": callback_url}})
 
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps({'url': url, 'callback_url': callback_url,  'blob_id': blob_id})
+            }
